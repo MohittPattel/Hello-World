8e22c36adc4c26de9f00022de37d3436c66b1f45
Mohit_Patel <MohittPattel@gmail.com>
2022-01-31 16:38:14 +0530

    remove the function header

--- math.py
+++ math.py
@@ -1,18 +1,11 @@
-# Add implementation
 def add(a, b):
     return a + b
-# Subtract implementation
 def suibtract(a, b):
-    return a - b   #on master branch
-# Multiply implementation
+    return a - b
 def multiply(a, b):
     return a * b 
-# Divide implementation
 def divide(a, b):
     if b == 0:
         return Divide_By_0
     else:
         a / b
-#swuare  implementtation
-def square(a):
-    return a * a
